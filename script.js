0fd0693aaff5c2556e56d9180fc528b14a04fba4
Anette's Library <kreatrice@proton.me>
2026-07-07 11:17:06 +0200

    Update script.js
    opdateret call-funktionen af json data

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-
 async function analyzeInput() {
 
-    const response =
-        await fetch("data/troubleshooting/problems.json");
+    const response = await fetch("data/troubleshooting/problems.json");
+    const problems = await response.json();
 
-    const problems =
-        await response.json();
+    const text = document
+        .getElementById("userInput")
+        .value
+        .toLowerCase();
 
-    console.log(problems);
+    for (const problem of problems) {
+
+        if (text.includes(problem.keyword)) {
+
+            document.getElementById("result").innerHTML =
+                problem.advice;
+
+            return;
+        }
+    }
+
+    document.getElementById("result").innerHTML =
+        "I don't know that problem yet.";
 
 }
