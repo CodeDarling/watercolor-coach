b2ee75c1fc6bc927a3797aa6aca540f279b7ab30
Anette's Library <kreatrice@proton.me>
2026-07-07 10:58:00 +0200

    Update script.js
    removed script til load af return input

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 
-
-function analyzeInput() {
-    let text = document.getElementById("userInput").value;
-
-    document.getElementById("result").innerHTML =
-        "You wrote: " + text;
-}
-
 for (let rule of problems) {
 
     if (text.includes(rule.keyword)) {
